638518498186fa76c7c9b16265f642b3988be2b2
asmat ahmadani <asmatyaar087@gmail.com>
2019-11-23 21:10:02 +0500

    final commit for edit button (option added to cancel operation)

--- public/browser.js
+++ public/browser.js
@@ -1,12 +1,14 @@
 document.addEventListener("click" , (e)=>{
    if (e.target.classList.contains("edit-me")){
-    let input = prompt("enter you values here")
-    console.log(input)
-    axios.post("/update", {text:input}).then(()=>{
-       //do something interesting here
-    }).catch(()=>{
-       console.log("error occured")
-    })
+    let input = prompt("enter you values here",e.target.parentElement.parentElement.querySelector(".item-text").innerHTML)
+    
+    if(input){
+      axios.post("/update", {text:input,id:e.target.getAttribute("data-id")}).then(()=>{
+         e.target.parentElement.parentElement.querySelector(".item-text").innerHTML=input
+      }).catch(()=>{
+         console.log("error occured")
+      })
+    }
    }
    
     
