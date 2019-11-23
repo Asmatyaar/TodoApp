f21a129ceb230bc74cfaf69fdadd6decef12a476
asmat ahmadani <asmatyaar087@gmail.com>
2019-11-23 20:59:04 +0500

    edit buttons enabled via axious

--- server.js
+++ server.js
@@ -50,7 +50,7 @@ app.get("/", (req , res)=>{
       <li class="list-group-item list-group-item-action d-flex align-items-center justify-content-between">
       <span class="item-text">${item.text}</span>
       <div>
-        <button class="edit-me btn btn-secondary btn-sm mr-1">Edit</button>
+        <button data-id="${item._id}" class="edit-me btn btn-secondary btn-sm mr-1">Edit</button>
         <button class="delete-me btn btn-danger btn-sm">Delete</button>
       </div>
     </li>`
@@ -76,5 +76,7 @@ app.post("/create", (req, res)=>{
   
 })
   app.post("/update", (req,res)=>{
-    console.log(req.body.text)
-  })
+   db.collection('items').findOneAndUpdate({_id: new mongodb.ObjectId(req.body.id)}, {$set:{text:req.body.text}}, ()=>{
+     res.send("success")
+   })
+}) 
